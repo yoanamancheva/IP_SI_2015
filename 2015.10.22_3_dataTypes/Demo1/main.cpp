b8396234c83b0a55baaab2ac3dc46ab97deb3a83
mimipaskova <maria.paskova3@gmail.com>
2015-10-21 00:37:34 +0300

    Add demo with overflow.

---
+++ 2015.10.22_3_dataTypes/Demo1/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+//  [-32768 ;32767]
+    short int a = 32760;
+    short int b = 32777;
+    short int c = a+b;
+    cout<<c;
+
+    return 0;
+}
